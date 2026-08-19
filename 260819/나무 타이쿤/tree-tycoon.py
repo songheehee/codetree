b941cefef47db3a1297bf2db010df1464eddc7f9
songheehee <74169534+songheehee@users.noreply.github.com>
2026-08-19 17:51:38 +0900

    [Passed] 나무 타이쿤 (75ms, 18MB)

--- 260819/나무 타이쿤/tree-tycoon.py
+++ 260819/나무 타이쿤/tree-tycoon.py
@@ -1,17 +1,19 @@
 # 영양제 : 나무 높이 + 1 / 씨앗 -> 높이 1 나무
 # 영양제 이동 시 행, 열 끝과 끝 연결되어있음
-
 # 1. 영양제 이동 규칙에 따라 이동
 # 2. 이동 시킨 뒤 땅에 영양제 투입 -> 영양제 사라짐
 # 3. 대각선으로 인접한 방향에 높이가 1 이상인 리브로수가 있는 만큼 높이 성장. 격자 벗어나면 안 셈
 # 4. 특수 영양제 맞은 곳 제외하고 높이 2 이상인 리브로수는 높이 -2, 해당 위치에 영양제
 
 def inject():
-    # 해당 위치 기준 +1,
+    # 해당 위치 기준 +1
     for r, c in medic:
         matrix[r][c] += 1
 
     # 3. 대각선으로 리브로수 있는 개수만큼 성장
+    ddr = [-1, -1, 1, 1]
+    ddc = [-1, 1, -1, 1]
+
     for r, c in medic:
         for d in range(4):
             nr = r + ddr[d]
@@ -20,13 +22,13 @@ def inject():
             if not (0 <= nr < n and 0 <= nc < n): # 격자 n,n 주의
                 continue
 
-            if matrix[nr][nc] >= 1: # 대각선에 있으면
+            if matrix[nr][nc]: # 대각선에 있으면
                 matrix[r][c] += 1 # 어차피 무조건 1 이상 오르기 때문에 한꺼번에 안 해줘도 됨
 
 def cut():
     global medic
     # 잘라내고 영양제 추가
-    new_medic = []
+    new_medic = set()
 
     for i in range(n):
         for j in range(n):
@@ -34,7 +36,7 @@ def cut():
                 continue
 
             if matrix[i][j] >= 2:
-                new_medic.append((i, j))
+                new_medic.add((i, j))
                 matrix[i][j] -= 2
 
     medic = new_medic # 영양제 바꿔주기
@@ -44,25 +46,16 @@ def cut():
 mdr = [0, -1, -1, -1, 0, 1, 1, 1]
 mdc = [1, 1, 0, -1, -1, -1, 0, 1]
 
-# 대각선
-ddr = [-1, -1, 1, 1]
-ddc = [-1, 1, -1, 1]
-
 n, m = map(int, input().split()) # 격자 크기 (3~15), 총 년 수 (1~100)
 matrix = [list(map(int, input().split())) for _ in range(n)] # 나무 높이
-moves = [tuple(map(int, input().split())) for _ in range(m)] # 각 년도의 이동 규칙 (방향, 칸수). 방향 -1 하기
-medic = [(n-1, 0), (n-2, 0), (n-1, 1), (n-2, 1)] # 영양제 위치. 최초 좌하
+medic = {(n-1, 0), (n-2, 0), (n-1, 1), (n-2, 1)} # 영양제 위치. 최초 좌하
 
-for d, p in moves: # 년마다 이동
-    d -= 1 # 방향, 칸수
+for _ in range(m): # 년마다 이동
+    d, p = map(int, input().split()) # 방향, 칸수
+    d -= 1
 
     # 1. 영양제 이동
-    for i in range(len(medic)):
-        mr, mc = medic[i]
-        mr = (mr + (mdr[d] * p)) % n
-        mc = (mc + (mdc[d] * p)) % n
-
-        medic[i] = (mr, mc)
+    medic = {((r + mdr[d] * p) % n, (c + mdc[d] * p) % n) for r, c in medic}
 
     # 2. 영양제 투입
     inject()

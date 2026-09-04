c1d5c9d04443d35397dbc72fee1121d99f47861f
songheehee <74169534+songheehee@users.noreply.github.com>
2026-09-04 11:54:45 +0900

    [Passed] 자율주행 전기차 (57ms, 17MB)

--- 260904/자율주행 전기차/autonomous-electric-car.py
+++ 260904/자율주행 전기차/autonomous-electric-car.py
@@ -21,6 +21,11 @@
 [실수]
     - 문제에 대놓고 목적지가 누군가의 출발지일 수 있다고 써져있었건만 2번 테케 보기 전까지 생각 못함 ㅎㅎ....
     - 엣지 여러개 생각해놓고 풀었는데도 승객이 남아있지만 태우지 못할 경우를 생각 못함...ㅠ 멍충이
+    - 같을 경우도 되는지 꼭 두번 세번 확인하자. 불안하면 = 빼자. 어차피 한번 더 돈다고 세상 안 망함
+    - 오...아예 시간 복잡도 생각 안 하고 풀었는데 운이 좋은거다...다음엔 꼭 보긴 보자
+
+[리팩토링]
+    - 처음에 하나의 bfs로 생각해서 ride를 search 안에다 넣었는데 생각해보니 따로 따로 구현해주는 게 더 나을 거 같긴 하다. 까먹지 말고 큰 틀 1, 2 짜자!
 '''
 # 승객 목적지에 무사히 도착하면 소모한 배터리 양의 두배만큼 충전
 # 배터리 다 쓰면 그 즉시 종료. 도착하고 다 소모 돼도 충전. 마지막 승객 내려준 후에도 충전
@@ -32,25 +37,22 @@
 from collections import deque
 
 def search():
-    global B
-
     # 현재 위치가 곧 출발지면
     if (r, c) in people:
-        return ride(r, c)
+        return (r, c, 0)
 
     visited = [[0] * N for _ in range(N)] # 거리 계산용
     q = deque([(r, c)])
     visited[r][c] = 1
 
     # 최단 거리 손님
-    pr, pc = -1, -1
-    dist = N*N
+    pr, pc, dist = -1, -1, N*N
 
     while q:
         cr, cc = q.popleft()
 
-        if B < visited[cr][cc] and (pr, pc) == (-1, -1): # 연료 불가. 손님 찾았는지도 확인해야됨
-            return False
+        if B <= visited[cr][cc] and (pr, pc) == (-1, -1): # 연료 불가. 손님 찾았는지도 확인해야됨
+            return (-1, -1, -1)
 
         for d in range(4):
             nr = cr + dr[d]
@@ -71,24 +73,16 @@ def search():
                 elif (dist, pr, pc) > (visited[cr][cc], nr, nc):
                     dist, pr, pc = visited[cr][cc], nr, nc
             else:
-                if visited[cr][cc] > dist: # 이미 최단 찾음. dist가 -1 상태라 같을 경우에도 끝
+                if visited[cr][cc] > dist: # 이미 최단 찾음. dist가 -1 상태라 같을 경우에도 끝 될 줄 알았는데 아닌가봄 ㅠ
                     break
 
                 q.append((nr, nc))
                 visited[nr][nc] = visited[cr][cc] + 1
 
-    # 택시 태우기
-    # 배터리 다 쓰면 종료. 태워도 충전되는게 아니라 목적지까지 가야돼서 0 되면 그냥 종료
-    B -= dist
-    if B <= 0:
-        return False
+    return pr, pc, dist
 
-    if (pr, pc) != (-1, -1): # 다 돌았는데도 손님 못 찾음
-        return ride(pr, pc)
 
 def ride(sr, sc): # 목적지 가기
-    global B, r, c
-
     visited = [[0] * N for _ in range(N)]
     q = deque([(sr, sc)]) # 손님 위치
     visited[sr][sc] = 1
@@ -110,19 +104,16 @@ def ride(sr, sc): # 목적지 가기
             if (nr, nc) == (er, ec): # 목적지 도착
                 # 연료 남아있는지
                 if B - visited[cr][cc] < 0: # 0이어도 괜찮음
-                    return False
+                    return (-1, -1, -1)
 
-                B += visited[cr][cc] # 충전
                 people.pop((sr, sc), None) # 손님 삭제
-                r, c = nr, nc # 자동차 위치 변경
+                return (nr, nc, visited[cr][cc])
 
-                return True
+            # 아직 목적지 도착 전
+            q.append((nr, nc))
+            visited[nr][nc] = visited[cr][cc] + 1
 
-            else: # 아직 목적지 도착 전
-                q.append((nr, nc))
-                visited[nr][nc] = visited[cr][cc] + 1
-
-    return False # 다 돌았는데도 목적지 못 찾음
+    return (-1, -1, -1) # 다 돌았는데도 목적지 못 찾음
 
 
 dr = [1, 0, -1, 0]
@@ -131,17 +122,31 @@ dc = [0, 1, 0, -1]
 N, P, B = map(int, input().split()) # 격자 크기, 승객 수, 초기 배터리
 matrix = [list(map(int, input().split())) for _ in range(N)] # 0 = 도로, 1 = 벽
 r, c = map(lambda x: int(x)-1, input().split()) # 자동차 현재 위치
-people = dict()
+people = dict() # 출발지 : 목적지
 
 for _ in range(P):
     sr, sc, er, ec = map(lambda x: int(x)-1, input().split())
     people[(sr, sc)] = (er, ec) # 출발지 다 다름
     
 while people:
-    possible = search()
+    # 1. 태울 최단 승객 찾기
+    pr, pc, dist = search()
 
-    if not possible:
+    # 배터리 다 쓰거나 손님 못 찾으면 종료
+    B -= dist
+    if B <= 0 or (pr, pc) == (-1, -1):
         B = -1
         break
+
+    # 2. 목적지까지 태우기
+    pr, pc, dist = ride(pr, pc)
+
+    if B <= 0 or (pr, pc) == (-1, -1):
+        B = -1
+        break
+
+    # 충전, 자동차 위치 변경
+    B += dist # 충전
+    r, c = pr, pc # 자동차 위치 변경
 
 print(B) # 남은 연료의 양. 모든 손님 이동 불가 -1

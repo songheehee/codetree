1ac46a58c7b267da7763bdef6b8b80e8bf8b035e
songheehee <74169534+songheehee@users.noreply.github.com>
2026-09-07 20:00:44 +0900

    [Passed] 미로 타워 디펜스 (151ms, 19MB)

--- 260907/미로 타워 디펜스/maze-tower-defense.py
+++ 260907/미로 타워 디펜스/maze-tower-defense.py
@@ -1,3 +1,27 @@
+'''
+소요시간 : 2시간 24분
+수행 시간 : 125ms / 메모리 : 19MB
+시도 : 2번
+
+이해 및 구상 (8분) - 구현 및 디버깅 (2시간 5분) - 디버깅 (11분)
+
+[구상]
+    - 아...난 진짜 달팽이가 너ㅜㅁ너무눠무너뭐눠머눠 싫어!!!!!!!
+    - 처음에 일단 공격부터 하고 음...당기는거 어케 하지 고민하다가 뒤에서부터 당길지, 앞에서부터 당길지 고민
+
+[구현]
+    - 몬스터를 1차원 배열로 만들자는 생각을 중간에 하기 시작했다; 그래서 중간에 수정함 띠용
+    - 처음 공격도 1차원으로 먼가 할 수 있을거 같은데 음 모르겠다! 그 부분은 이미 짜놓은 상태라서 수정하지 않음 ㅎ
+    - 처음엔 달팽이 돌면서 앞에서 당길지 뒤에서 당길지 하다가 도대체 여러번 당기는 걸 어떻게 짜는지 모르겠는 거임. 그래서 그냥 순서대로 받고 다시 그려야겠다! 근데 그냥 애초부터 1차원으로 했어도 됐을듯
+
+[실수]
+    - 아니 draw에서 똑같이 마지막꺼 해줬으면서 왜 delete에선 안 해줬냐...멍충이
+
+[리팩토링]
+    - 달팽이 좌표를 다 받고 시작하는 것도 좋겠다
+    - 다시 그릴 필요 없게 애초부터 1차원으로 했어도 좋았을 듯. 근데 그럼 공격 때 어케 하지?
+    - delete 랑 draw 때 쓸데없이 마지막꺼 따로 처리 안 하는 방법 없나?
+'''
 # 3. 몬스터 종류가 네번 이상 연속되면 삭제 (동시에. 똥글뱅이) -> 점수
 def delete(monsters):
     global score
@@ -6,6 +30,7 @@ def delete(monsters):
     count = 1
     delete_set = set()
     si = 0
+
     for i in range(len(monsters)):
         mon = monsters[i]
 
@@ -71,13 +96,11 @@ def move():
             scount = 0
             snum += 1
 
-    # 지운 애들 다시 그려주기
-    draw(delete(monsters))
+    return monsters
 
 
 # 4. 몬스터 차례대로 나열했을 때 같은 숫자끼리 짝 -> (총 개수, 숫자)로 바꿔서 다시 미로. 격자 크기까지만
 def draw(monsters):
-    global matrix
     # 새로운 몬스터 배열
     new_monsters = []
     prev = 0
@@ -129,7 +152,7 @@ def draw(monsters):
             scount = 0
             snum += 1
 
-    matrix = new_matrix
+    return new_matrix
 
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
@@ -157,6 +180,13 @@ for _ in range(R):
         matrix[nr][nc] = 0
 
     # 2. 비어있는 공간만큼 몬스터 앞으로 이동하여 공간 채움 (똥글뱅이)
-    move()
+    monsters = move()
+
+    # 연속된 몬스터 삭제
+    monsters = delete(monsters)
+
+    # 새로 그려주기
+    matrix = draw(monsters)
+
 
 print(score) # 얻게되는 점수

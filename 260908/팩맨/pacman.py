80cbed61fc29a8ff6ef091eda0b360e754473dae
songheehee <74169534+songheehee@users.noreply.github.com>
2026-09-08 16:51:06 +0900

    [Passed] 팩맨 (55ms, 16MB)

--- 260908/팩맨/pacman.py
+++ 260908/팩맨/pacman.py
@@ -1,31 +1,3 @@
-'''
-소요시간 : 1시간 23분
-수행 시간 : 52ms / 메모리 : 16MB
-
-이해 및 구상 (16분) - 구현 및 디버깅 (1시간 7분)
-
-[구상]
-    - 또 이동 문제가 나왔따리. dict로 할까 리스트로 할까 나는 딕셔너리 파이긴 한데 알, 시체 많으니까 몬스터는 리스트로 할까
-    - 처음에 매트릭스 크기가 안 나와있어서 딕셔너리로 하려다가 맨 첫 줄에;; 4*4 라길래 작아서 매트릭스로 바꿈
-    - 딕셔너리로 하려고 할 때는 몬스터랑 알을 같은 딕셔너리로 처리해줄까 하다가 어차피 리스트로 해서 따로 처리해줬다
-    - 그럼 이제 시체랑 알을 같은 딕셔너리로 해줄까 하다가 헷갈리니까 그냥 따로 처리해줌
-    - 몬스터, 시체, 알 다 한 칸에 여러개 들어갈 수 있음!!! 유의
-    - 혹시 몰라서 몬스터 초기 위치랑 팩맨 초기 위치 같을 수 있다는 거 적어놨는데 딱히 신경 안 써도 될듯?
-    - 대신 팩맨이 이동 전에 몬스터랑 같이 있어도 얘는 안 먹는다는 거 인지했음
-
-[구현]
-    - 문제에서 아주 친절하게 1번 2번 이렇게 써놔줘서 고대로 따라갔다
-    - 팩맨 이동할 때 격자 밖으로 나가는 건 ㄴㄴ 상하좌우 중에 3개 중복으로 고르는 거라 순열로 풀었다. 난 부분집합으로 푼 줄 알았는데 지금 보니까 순열이네; 띠용
-    - 리스트도 min 되나 잠깐 디버깅에서 확인해봄 -> 된다!
-    - 처음에 원복시켜주는거 귀찮아서 바로 len(matrix[nr][nc]) 더해주는걸로 했는데 그럼 왔다갔다 하면서 중복 카운트 될 수 있음; 하 귀찮아...
-    - nr, nc가 pr, pc 가 아니어야 하는데 or 인지 and 인지 헷갈려서 그냥 튜플로 비교...or 일 거 같음
-    - 헷갈렸던 건 시체 소멸이 이번 시체가 이번에 소멸인지 아닌지 헷갈...근데 예시 보니까 다다음까지 살아있길래 그냥 애초에 3으로 줌
-
-[리팩토링]
-    - 어차피 팩맨 이동 시 우선순위부터 보기 때문에 같을 경우 생각할 필요 없음
-    - egg 딕셔너리 만들지 않고 그냥 복사하면 되잖아...?? 세상에 생각도 못했다;
-    - 시체 딕셔너리가 낫나...리스트가 낫나...
-'''
 # 몬스터 상하좌우, 대각선
 # 1. 몬스터 복제 시도 - 현재 몬스터 위치에서 같은 방향 알
 # 2. 몬스터 이동 - 몬스터 시체/팩맨/격자 벗어남 -> 반시계 45도 회전 가능할때까지. 다 못가면 이동하지 않음
@@ -74,7 +46,7 @@ def monster_move():
                     nr = i + ddr[nd]
                     nc = j + ddc[nd]
 
-                    if (0 <= nr < 4 and 0 <= nc < 4) and (nr, nc) != (pr, pc) and (nr, nc) not in dead:
+                    if (0 <= nr < 4 and 0 <= nc < 4) and (nr, nc) != (pr, pc) and not dead[nr][nc]:
                         d = nd
                         break
 
@@ -94,8 +66,8 @@ ddc = [0, -1, -1, -1, 0, 1, 1, 1]
 
 M, T = map(int, input().split()) # 몬스터 마리 수, 턴 수. 10, 25
 pr, pc = map(lambda x: int(x)-1, input().split()) # 팩맨 위치
-matrix = [[[] for _ in range(4)] for _ in range(4)] # 여러마리 일 수 있음
-dead = dict() # 시체. 여러개 가능
+matrix = [[[] for _ in range(4)] for _ in range(4)] # 몬스터 여러마리 일 수 있음
+dead = [[[] for _ in range(4)] for _ in range(4)] # 시체. 여러개 가능
 
 for _ in range(M):
     R, C, D = map(lambda x: int(x)-1, input().split())
@@ -120,27 +92,23 @@ for _ in range(T):
         pc += dc[d]
 
         if matrix[pr][pc]:
-            count = len(matrix[pr][pc]) # 몬스터 마리 수
+            dead[pr][pc].extend([3] * len(matrix[pr][pc])) # 몬스터 마리 수만큼
             matrix[pr][pc] = []
 
-            if (pr, pc) in dead:
-                dead[(pr, pc)].extend([3] * count) # 2턴 동안 살아있음. 이번 턴 포함인겨..?
-            else:
-                dead[(pr, pc)] = [3] * count
-
     # 5. 시체 소멸
-    for k, v in list(dead.items()):
-        new_dead = []
+    for i in range(4):
+        for j in range(4):
+            new_dead = []
 
-        for left in v: # 한턴 빼주기
-            left -= 1
-            if left:
-                new_dead.append(left)
+            for left in dead[i][j]:
+                left -= 1
+                if left:
+                    new_dead.append(left)
 
-        if new_dead:
-            dead[k] = new_dead
-        else:
-            dead.pop(k)
+            if new_dead:
+                dead[i][j] = new_dead
+            else:
+                dead[i][j] = []
 
     # 6. 알 부화
     for i in range(4):

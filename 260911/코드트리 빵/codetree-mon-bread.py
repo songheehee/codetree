7dfdb19cc8fe1fffb9e84c6fc5d3693c4220b09c
songheehee <74169534+songheehee@users.noreply.github.com>
2026-09-11 12:07:36 +0900

    [Passed] 코드트리 빵 (56ms, 17MB)

--- 260911/코드트리 빵/codetree-mon-bread.py
+++ 260911/코드트리 빵/codetree-mon-bread.py
@@ -1,4 +1,4 @@
-# 자기 번호만큼 M번에 출발 -> 편의점 이동. 목표 편의점 다 다름
+# 자기 번호 때 출발 -> 편의점 이동. 목표 편의점 다 다름
 # 1. 격자에 있는 경우, 편의점 방향을 향해 1칸 이동. 최단 거리로 이동. 위왼오아 우선
 # 2. 격자에 있는 경우, 편의점에 도착하면 멈춤. 다른 사람들 못 지나감. 해당 시간에 모두 이동한 뒤에
 # 3. T <= M 만족한다면 편의점과 가장 가까운 베이스캠프 들어감. 최단거리 같은 경우에는 행작, 열작
@@ -14,44 +14,55 @@ from collections import deque
 
 def bfs(r, c, i):
     visited = [[0] * N for _ in range(N)]
-    q = deque([(r, c, [])])
+    q = deque() # 첫번째로 디딘 칸 가지고 다니기
     visited[r][c] = 1
 
+    for d in range(4):
+        nr = r + dr[d]
+        nc = c + dc[d]
+
+        if not (0 <= nr < N and 0 <= nc < N and matrix[nr][nc] >= 0):
+            continue
+
+        if (nr, nc) == store[i]:  # 자기 편의점 도착
+            return nr, nc
+
+        visited[nr][nc] = 1
+        q.append((nr, nc, (nr, nc)))
+
     while q:
-        for _ in range(len(q)):
-            cr, cc, path = q.popleft()
+        cr, cc, first = q.popleft()
 
-            for d in range(4):
-                nr = cr + dr[d]
-                nc = cc + dc[d]
+        for d in range(4):
+            nr = cr + dr[d]
+            nc = cc + dc[d]
 
-                if not (0 <= nr < N and 0 <= nc < N):
-                    continue
+            if not (0 <= nr < N and 0 <= nc < N):
+                continue
 
-                if visited[nr][nc] or matrix[nr][nc] < 0:  # 못 가는 길
-                    continue
+            if visited[nr][nc] or matrix[nr][nc] < 0:  # 못 가는 길
+                continue
 
-                if (nr, nc) == store[i]:  # 자기 편의점 도착
-                    return path[0] if path else (nr, nc)
+            if (nr, nc) == store[i]:  # 자기 편의점 도착
+                return first
 
-                visited[nr][nc] = visited[cr][cc] + 1
-                q.append((nr, nc, path+[(nr, nc)]))
+            visited[nr][nc] = 1
+            q.append((nr, nc, first))
 
 
 def move():
-    global new_matrix
     new_matrix = [row[:] for row in matrix] # 다 이동한 뒤에 갈아끼기
 
     for i in range(1, min(turn+1, P+1)):
         if not player[i]:
             continue
 
-        (pr, pc) = bfs(*player[i], i) # 플레이어 새 위치
+        pr, pc = bfs(*player[i], i) # 플레이어 새 위치. 받을 때 튜플로 받아야됨
 
         if (pr, pc) == store[i]:  # 자기 편의점 도착
             new_matrix[pr][pc] = -2
             player[i] = 0
-        else:
+        else: # 다음 위치로 이동
             player[i] = pr, pc
 
     return new_matrix
@@ -100,9 +111,7 @@ player = [0] * (P+1)
 store = [0] # 편의점이랑 플레이어 번호 매핑
 
 for _ in range(P):
-    x, y = map(int, input().split())
-    x -= 1
-    y -= 1
+    x, y = map(lambda x: int(x)-1, input().split())
 
     matrix[x][y] = 2 # 편의점 표시. 굳이 안 해도 될거 같기도 하고
     store.append((x, y))
